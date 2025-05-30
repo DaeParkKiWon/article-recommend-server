52d104866bbc2a593f573d60c4db37104144631f
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2025-05-30 17:13:50 +0900

    feat : 서버 초기 세팅
    - 서버 초기 세팅

---
+++ src/main/java/basecampus/service/article_recommend/ArticleRecommendApplication.java
@@ -0,0 +1,13 @@
+package basecampus.service.article_recommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleRecommendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleRecommendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/basecampus/service/article_recommend/ArticleRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package basecampus.service.article_recommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleRecommendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
