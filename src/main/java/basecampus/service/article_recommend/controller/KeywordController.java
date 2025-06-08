2506468a240a6d7591e4144f5f4428acf2ae4831
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-06-08 17:41:19 +0900

    feat: 최종 변경적용

--- src/main/java/basecampus/service/article_recommend/controller/KeywordController.java
+++ src/main/java/basecampus/service/article_recommend/controller/KeywordController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/keywords")
+@RequestMapping("/api/keywords")
 class KeywordController {
 
     private final KeywordService keywordService;
